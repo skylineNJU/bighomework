b17ae25799ebc25f3eeab033dfa6ecce2423af9b
FWWorks <214731923@qq.com>
2015-10-26 19:46:23 +0800

    load与receipt驱动

--- SkylineClient/src/data_driver/LoadDataService_Driver.java
+++ SkylineClient/src/data_driver/LoadDataService_Driver.java
@@ -1,5 +1,38 @@
 package data_driver;
 
-public class LoadDataService_Driver {
+import java.util.List;
 
+import po.VehicleLoadingPO;
+import dataservice.LoadDataService;
+
+public class LoadDataService_Driver {
+public void drive(LoadDataService loadDataService){
+	if(loadDataService.writeVehicleLoadBase(null, "12345678")){
+		System.out.println("---------Writing VehicleLoadBase Success!-------------");
+			}
+	if(loadDataService.readVehicleLoadBase()!=null){
+		System.out.println("---------Reading VehicleLoadBase Success!-------------");
+	}
+	if(loadDataService.delVehicleLoadBase(null)){
+		System.out.println("---------Deleting VehicleLoadBase Success!-------------");
+	}
+	if(loadDataService.writeTrainLoadBase(null, "123456789")){
+		System.out.println("---------Writing TrainLoadBase Success!-------------");
+	}
+	if(loadDataService.readTrainLoadBase()!=null){
+		System.out.println("---------Reading TrainLoadBase Success!-------------");
+	}
+	if(loadDataService.delTrainLoadBase(null)){
+		System.out.println("---------Deleting TrainLoadBase Success!-------------");
+	}
+	if(loadDataService.writePlaneLoadBase(null, "123456789")){
+		System.out.println("---------Writing PlaneLoadBase Success!-------------");
+	}
+	if(loadDataService.readPlaneLoadBase()!=null){
+		System.out.println("---------Reading PlaneLoadBase Success!-------------");
+	}
+	if(loadDataService.delPlaneLoadBase(null)){
+		System.out.println("---------Deleting PlaneLoadBase Success!-------------");
+	}
+}
 }
