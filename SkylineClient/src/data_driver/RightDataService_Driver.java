c6be6be3223424a08ef28d0338d884066a8b0813
yuinii1215 <hq14@software.nju.edu.cn>
2015-10-28 20:50:55 +0800

    Driver

--- SkylineClient/src/data_driver/RightDataService_Driver.java
+++ SkylineClient/src/data_driver/RightDataService_Driver.java
@@ -1,5 +1,28 @@
 package data_driver;
 
+import po.AccountPO;
+import po.Rights;
+import dataservice.RightDataService;
+
 public class RightDataService_Driver {
 
+	public boolean rightDataDrive(RightDataService rightDataService){
+		
+		String account = "1232456";
+		AccountPO accountPO = new AccountPO("1212525", "1515", null);
+		
+		if(rightDataService.initKeyWord(account)){
+			System.out.println("----initKeyWord succeed!----");
+		}
+		if(rightDataService.delAccount(account)){
+			System.out.println("----delAccount succeed!----");
+		}
+		if(rightDataService.changeRight(accountPO)){
+			System.out.println("----changeRight succeed!----");
+		}
+		if(rightDataService.readRight(account)!=null){
+			System.out.println("----readRight succeed!----");
+		}
+		return true;
+	}
 }
--- SkylineClient/src/data_driver/WarehouseDataService_Driver.java
+++ SkylineClient/src/data_driver/WarehouseDataService_Driver.java
@@ -1,5 +1,26 @@
 package data_driver;
 
-public class WarehouseDataService_Driver {
+import po.WarehouseInPO;
+import po.WarehouseOutPO;
+import dataservice.WarehouseDataService;
 
+public class WarehouseDataService_Driver {
+ 
+	public boolean warhouseDrive(WarehouseDataService warehouseDataService){
+		
+		WarehouseInPO warehouseInPO = new WarehouseInPO("123456789", "2015/02/26", "北京", "A", 1, 2, 3);
+		WarehouseOutPO warehouseOutPO = new WarehouseOutPO("123456789", "2015/02/23", "北京", null, "2012151", "201515");
+		String warehouseNumber = "20151515";
+		
+		if(warehouseDataService.storage(warehouseInPO)){
+			System.out.println("----storage succeed!-----");
+		}
+		if(warehouseDataService.outWarehouse(warehouseOutPO)){
+			System.out.println("----outWarehouse succeed!-----");
+		}
+		if(warehouseDataService.readWarehouse(warehouseNumber)!=null){
+			System.out.println("----readWarehouse succeed!-----");
+		}
+		return true;
+	} 
 }
