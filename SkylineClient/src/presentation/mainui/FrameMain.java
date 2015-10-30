b0c630f66833f6c1d960c5c7462105b3fa105e92
TLOSP <dcf14@software.nju.edu.cn>
2015-10-30 16:42:45 +0800

    frame

--- SkylineClient/src/presentation/mainui/FrameMain.java
+++ SkylineClient/src/presentation/mainui/FrameMain.java
@@ -1,112 +1,102 @@
 package presentation.mainui;
-import java.awt.Dimension;
-import java.awt.Graphics;
+import java.awt.Frame;
 import java.awt.Point;
-import java.awt.Toolkit;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
-import java.awt.event.MouseMotionAdapter;
-import java.awt.event.MouseMotionListener;
 
-import javax.swing.JFrame;
+import javax.swing.*;
+import java.awt.event.*;
 
-//自定义的边框和退出、最小化、最大化的按钮
+@SuppressWarnings("serial")
 public class FrameMain extends JFrame{
+	private static JLabel exit=new JLabel(AllImage.exitImg);
+	private static JLabel hide=new JLabel(AllImage.smallestImg);
+	private static JLabel title=new JLabel(AllImage.edgW);
+	private static JPanel panel=new JPanel();
+	private static FrameMain frame;
+	private static boolean move=false;
+	private static Point origin = new Point();
+	private static java.awt.Dimension scrSize = java.awt.Toolkit.getDefaultToolkit().getScreenSize();
+	private FrameMain(){
 	
-	//窗口的原点X坐标
-	public int startX = 0;
-	//窗口的原点Y坐标
-	public int startY = 0;
-	public int frameWidth;
-	public int frameHeight;
-	//每个Lanel之间的间隔
-	public static final int INTERSPACE = 1;
-	//边框的宽度
-	public static final int PADDING = 7;
-	//Label的大小
-	public static final int LABEL_SIZE = 20;
-	
-	LabelExit labelExit;
-	LabelMin labelMin;
-	Point nowPoint = null;
-	Point newPoint = null;
-	
-	
-	public void initWH(){
-		Toolkit toolkit=Toolkit.getDefaultToolkit();
-	    Dimension screen=toolkit.getScreenSize();
-	    frameWidth = screen.width>>1;
-	    frameHeight = screen.height>>1; 
-		this.setBounds(startX, startY, frameWidth, frameHeight);	
-		this.setLayout(null);//布局设置为自定义布局	
-		this.setUndecorated(true);//去掉边框
 	}
 	
-	//描绘边框，安装Label，注册监听
-	//思考：Laebl其实可以写成一个类，通过new对象来实现
-	public void initLabel(){
+	public static void init(){
+		frame.setSize((int)((double)scrSize.getHeight()*3/4/490*724),(int)((double)scrSize.getHeight()*3/4));
+		frame.setLocation((int)(scrSize.getWidth()/2-(int)((double)scrSize.getHeight()*3/4/490*724/2))
+				,(int)(scrSize.getHeight()/2-(int)((double)scrSize.getHeight()*3/4/2)));
+		frame.setUndecorated(true);
+		frame.setResizable(false);
+		System.out.println(frame.getHeight());
+		exit.setSize(30,25);
+		hide.setSize(30,25);
+		title.setSize(frame.getWidth()-exit.getWidth()-hide.getWidth(),25);
+		title.setLocation(0,0);
+		exit.setLocation(frame.getWidth()-exit.getWidth(),0);
+		hide.setLocation(frame.getWidth()-2*hide.getWidth(),0);
+		System.out.println(exit.getWidth());
+		frame.add(panel);
+		panel.setLayout(null);
+		panel.add(exit);
+		panel.add(hide);
+		panel.add(title);
+		frame.addMouseListener(new MouseAdapter() {
+            public void mousePressed(MouseEvent e) {  //按下（mousePressed 不是点击，而是鼠标被按下没有抬起）
+            	 if(e.getY()<=30){
+            		 move=true;
+            	origin.x = e.getX();  //当鼠标按下的时候获得窗口当前的位置
+                origin.y = e.getY();
+            	 }
+            }
+            public void mouseReleased(MouseEvent e){ 
+				   move=false;
+			    } 
+          
+		});
+		frame.addMouseMotionListener(new MouseMotionAdapter() {
+            public void mouseDragged(MouseEvent e) {  //拖动（mouseDragged 指的不是鼠标在窗口中移动，而是用鼠标拖动）
+            	if(move){
+                Point p = frame.getLocation();  //当鼠标拖动时获取窗口当前位置
+                //设置窗口的位置
+                //窗口当前的位置 + 鼠标当前在窗口的位置 - 鼠标按下的时候在窗口的位置
+               
+                frame.setLocation(p.x + e.getX() - origin.x, p.y + e.getY() - origin.y);
+            	}
+            }
+		});
+		exit.addMouseListener(new MouseAdapter(){
+			public void mouseClicked(MouseEvent e){
+				System.exit(0);
+			}
+			
+			public void mouseEntered(MouseEvent e){
+				exit.setIcon(AllImage.exitEnterImg);
+			}
+			
+			public void mouseExited(MouseEvent e){
+				exit.setIcon(AllImage.exitImg);
+			}
+		});
+		hide.addMouseListener(new MouseAdapter(){
+			public void mouseClicked(MouseEvent e){
+				frame.setExtendedState(Frame.ICONIFIED);
+			}
+			public void mouseEntered(MouseEvent e){
+				hide.setIcon(AllImage.smallestEnterImg);
+			}
+			
+			public void mouseExited(MouseEvent e){
+				hide.setIcon(AllImage.smallestImg);
+			}
+		});
+		frame.setVisible(true);
+	}
+	public static void createFrame(){
+		frame=new FrameMain();
+	}
+	
+	public static void main(String args[]){
+		FrameMain.createFrame();
+		FrameMain.init();
+	}
 		
-		labelExit = new LabelExit(AllImage.exitImg, AllImage.exitEnterImg,this.getWidth()-LABEL_SIZE-PADDING, PADDING, LABEL_SIZE, LABEL_SIZE);
-		labelMin = new LabelMin(AllImage.smallestImg,AllImage.smallestEnterImg,this.getWidth()-LABEL_SIZE*2-INTERSPACE*2-PADDING, PADDING, LABEL_SIZE, LABEL_SIZE,this);
-		
-		LabelEdge[] labelEdge = new LabelEdge[4];
-		
-		labelEdge[0] = new LabelEdge(AllImage.frameEdgingWImg,0, 0, frameWidth, PADDING);
-		labelEdge[1] = new LabelEdge(AllImage.frameEdgingWImg,0, frameHeight-PADDING, frameWidth, PADDING);
-		labelEdge[2] = new LabelEdge(AllImage.frameEdgingHImg,0, 0, PADDING, frameHeight);
-		labelEdge[3] = new LabelEdge(AllImage.frameEdgingHImg,frameWidth-PADDING, 0, PADDING, frameHeight);
-		
-		this.getContentPane().add(labelMin);
-		this.getContentPane().add(labelExit);
-		
-		for(int i = 0;i<4;i++){
-			this.getContentPane().add(labelEdge[i]);
-		}
-		labelEdge[0].addMouseListener(new labelEdgeListener());
-		labelEdge[0].addMouseMotionListener(new edgeMoveListener());
-	}
-	
-	public FrameMain(){
-		initWH();		
-		initLabel();
-		this.setVisible(true);	
-	}
-	
-	public void updateFrame(Graphics g){
-		this.setBounds(startX, startY, frameWidth, frameHeight);	
-	}
-	
-	public static void main(String[] args) {
-		FrameMain mainFrame = new FrameMain();
-	}
 
-	//鼠标按下的监听器
-	class labelEdgeListener implements MouseListener{
-		public void mouseClicked(MouseEvent e) {}
-
-		public void mouseEntered(MouseEvent e) {}
-
-		public void mouseExited(MouseEvent e) {}
-
-		public void mousePressed(MouseEvent e) {
-			nowPoint = new Point(e.getX(),e.getY());
-		}
-
-		public void mouseReleased(MouseEvent e) {}
-	}
-	
-	class edgeMoveListener implements MouseMotionListener {
-
-		public void mouseDragged(MouseEvent e) {
-			newPoint = new Point(e.getX(),e.getY());
-			startX = (int) (startX + newPoint.getX() - nowPoint.getX());
-			startY = (int) (startY + newPoint.getY() - nowPoint.getY());
-			Graphics g = getGraphics();
-			updateFrame(g);
-		}
-		public void mouseMoved(MouseEvent e) {}
-		
-	}
 }
