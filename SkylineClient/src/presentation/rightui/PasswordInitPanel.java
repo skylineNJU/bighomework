334e6f3fda26e3144815c1dde7fb4fd9866c709a
FWWorks <214731923@qq.com>
2015-11-09 11:33:46 +0800

    额

--- SkylineClient/src/presentation/rightui/PasswordInitPanel.java
+++ SkylineClient/src/presentation/rightui/PasswordInitPanel.java
@@ -1,12 +1,23 @@
 package presentation.rightui;
 
+import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 import presentation.mainui.FrameMain;
 import presentation.mainui.MainController;
 
 public class PasswordInitPanel {
 	private JPanel panel;
+	private JLabel accountNameLabel;
+	private JLabel newPasswordLabel;
+	private JLabel newPasswordConfirmLabel;
+	private JTextField accountName;
+	private JTextField newPassword;
+	private JTextField newPasswordConfirm;
+	private JButton submit;
+	private JButton cancel;
 	
 	public PasswordInitPanel(){
 		panel=MainController.getWritepanel();
