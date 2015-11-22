826a3faab13516963d250bac11ac4381ccd5e5b3
yuinii1215 <hq14@software.nju.edu.cn>
2015-11-22 21:38:12 +0800

    Moditify

--- SkylineClient/src/main/presentation/warehouseui/ShowInventoryPanel.java
+++ SkylineClient/src/main/presentation/warehouseui/ShowInventoryPanel.java
@@ -12,9 +12,9 @@ import main.presentation.mainui.MainController;
 public class ShowInventoryPanel {
 
 	private JPanel panel;
+	private JPanel inInfo;
+	private JPanel outInfo;
 	private JTabbedPane tab;
-	private JTextField inInfo;
-	private JTextField outInfo;
 	private JTextField all;
 	private JTextField inOfNum;
 	private JTextField outOfNum;
@@ -47,24 +47,36 @@ public class ShowInventoryPanel {
 	}
 	
 	public void init(){
+		
+		inInfo = new JPanel();
+		outInfo = new JPanel();
+		inInfo.setLayout(null);
+		outInfo.setLayout(null);
+		
 		tab=new JTabbedPane(JTabbedPane.TOP);
 		tab.addTab("入库信息", inInfo);
 		tab.addTab("出库信息", outInfo);
 		tab.setSize(panel.getWidth()*7/8,panel.getHeight()/100*80);
 		tab.setLocation(panel.getWidth()/18,panel.getHeight()*3/20);
-
-		
-		Num();
-		dataPanel();
-		tablePanel();
+	
 		panel.add(tab);
+		
+		datePanel();
+		Num();
+		tablePanel();
+	
+		
+		inInfo.setVisible(true);
+		outInfo.setVisible(true);
+		
+		
 		tab.repaint();
 		panel.repaint();
-
+	
 	}
 	
 	
-	public void dataPanel(){
+	public void datePanel(){
 		startdate = new JLabel("开始日期:");
 		enddate = new JLabel("截止日期:");
 		Font font = new Font("宋体", Font.BOLD, 15);
@@ -276,76 +288,79 @@ public class ShowInventoryPanel {
 	}
 	
 	public void tablePanel(){
+		
 		title1="入库单号";
 		title2 ="出库单号";
 		title3="存储位置";
-	/*	String[] t1 = new String[]{title1,title3};
+		String[] t1 = new String[]{title1,title3};
 		String[] t2 = new String[]{title2,title3};
+
 		
-		table2 = new JTable(null,t1);
-	
+		String[] ot = new String[]{"入库单号","区号","排号","架号","位号"};
+		String[][] tl = new String[][]{{"111010000000","A","1","2","1"},{"111010000000","A","1","3","1"},{"111010000000","A","1","4","1"},
+				{"111010000200","B","2","2","1"},{"111010000050","K","1","2","1"},{"111010000090","F","1","2","1"},{"111010000000","A","1","9","1"},
+				{"111010000004","A","9","3","1"},{"111010000000","D","1","2","1"},{"111230000230","A","1","2","1"},{"111010000000","C","1","2","1"},
+				{"112020033000","H","11","2","7"},{"112040000003","D","1","2","1"},{"112310000010","B","2","3","1"},{"111010000000","C","2","1","1"}};
+		table1 = new JTable(tl,ot);
+		String[] ot1 = new String[]{"出库单号","区号","排号","架号","位号"};
+		table2 = new JTable(tl,ot1);
+	//in
+		table1.setEnabled(false);//设置不可编辑内容
+		table1.setRowHeight(panel.getWidth()/20);//设置列宽
+		table1.getTableHeader().setPreferredSize(new Dimension(1, panel.getWidth()/20));//设置表头高度
+		table1.getTableHeader().setReorderingAllowed(false);//表头不可移动
+		table1.setDragEnabled(false);
+		table1.setVisible(true);
+		scrollPane = new JScrollPane(table1);
+
+		if(tl.length>=10){
+			scrollPane.setBounds(tab.getX()-panel.getWidth()/18, tab.getY()-panel.getWidth()/8,tab.getWidth(), 10*table1.getRowHeight());
+		}else{
+			scrollPane.setBounds(tab.getX()-panel.getWidth()/18, tab.getY()-panel.getWidth()/8,tab.getWidth(), (table1.getRowCount()+1)*table1.getRowHeight());
+		}
+		table1.getColumnModel().getColumn(0).setPreferredWidth(scrollPane.getWidth()/5);;
+		table1.getColumnModel().getColumn(1).setPreferredWidth(scrollPane.getWidth()/6);;
+		table1.getTableHeader().setResizingAllowed(false);//设置列宽不可变
+		scrollPane.setVisible(true);
+		inInfo.add(scrollPane);
+	//out
 		table2.setEnabled(false);//设置不可编辑内容
 		table2.setRowHeight(panel.getWidth()/20);//设置列宽
 		table2.getTableHeader().setPreferredSize(new Dimension(tab.getWidth(), panel.getWidth()/20));//设置表头高度
 		table2.getTableHeader().setReorderingAllowed(false);//表头不可移动
 		table2.setDragEnabled(false);
 		table2.setVisible(true);
-		scrollPane2 = new JScrollPane(table1);
+		scrollPane2 = new JScrollPane(table2);
 
-		scrollPane2.setBounds(tab.getX(), tab.getY()+panel.getHeight()/24,tab.getWidth(),table1.getRowHeight());
-
+		if(tl.length>=10){
+			scrollPane2.setBounds(tab.getX()-panel.getWidth()/18, tab.getY()-panel.getWidth()/8, tab.getWidth(), 10*table1.getRowHeight());
+		}else{
+			scrollPane2.setBounds(tab.getX()-panel.getWidth()/18, tab.getY()-panel.getWidth()/8,tab.getWidth(), (table1.getRowCount()+1)*table1.getRowHeight());
+		}
 		table2.getColumnModel().getColumn(0).setPreferredWidth(scrollPane.getWidth()/5);;
 		table2.getColumnModel().getColumn(1).setPreferredWidth(scrollPane.getWidth()/6);;
 		table2.getTableHeader().setResizingAllowed(false);//设置列宽不可变
 		table2.setVisible(true);
 		scrollPane2.setVisible(true);
-		panel.add(scrollPane2);
-	/*	String[] ot = new String[]{"区号","排号","架号","位号"};
-		String[][] tl = new String[][]{{"A","1","2","1"},{"A","1","3","1"},{"A","1","4","1"},
-				{"B","2","2","1"},{"K","1","2","1"},{"F","1","2","1"},{"A","1","9","1"},
-				{"A","9","3","1"},{"D","1","2","1"},{"A","1","2","1"},{"C","1","2","1"},
-				{"H","11","2","7"},{"D","1","2","1"},{"B","2","3","1"},{"C","2","1","1"}};
-		*/
-		String[] ot = new String[]{"","区号","排号","架号","位号"};
-		String[][] tl = new String[][]{{"111010000000","A","1","2","1"},{"111010000000","A","1","3","1"},{"111010000000","A","1","4","1"},
-				{"111010000200","B","2","2","1"},{"111010000050","K","1","2","1"},{"111010000090","F","1","2","1"},{"111010000000","A","1","9","1"},
-				{"111010000004","A","9","3","1"},{"111010000000","D","1","2","1"},{"111230000230","A","1","2","1"},{"111010000000","C","1","2","1"},
-				{"112020033000","H","11","2","7"},{"112040000003","D","1","2","1"},{"112310000010","B","2","3","1"},{"111010000000","C","2","1","1"}};
-		table1 = new JTable(tl,ot);
-	
-		table1.setEnabled(false);//设置不可编辑内容
-		table1.setRowHeight(panel.getWidth()/20);//设置列宽
-		table1.getTableHeader().setPreferredSize(new Dimension(tab.getWidth(), panel.getWidth()/20));//设置表头高度
-		table1.getTableHeader().setReorderingAllowed(false);//表头不可移动
-		table1.setDragEnabled(false);
-		table1.setVisible(true);
-		scrollPane = new JScrollPane(table1);
-		if(tl.length>=10){
-			scrollPane.setBounds(tab.getX(), tab.getY()+panel.getHeight()/24+table1.getRowHeight(), tab.getWidth(), 9*table1.getRowHeight());
-		}else{
-			scrollPane.setBounds(tab.getX(), tab.getY()+panel.getHeight()/24+table1.getRowHeight(),tab.getWidth(), (table1.getRowCount()+1)*table1.getRowHeight());
-		}
-		table1.getColumnModel().getColumn(0).setPreferredWidth(scrollPane.getWidth()/5);;
-		table1.getColumnModel().getColumn(1).setPreferredWidth(scrollPane.getWidth()/6);;
-		table1.getTableHeader().setResizingAllowed(false);//设置列宽不可变
-		scrollPane.setVisible(true);
-		panel.add(scrollPane);
+		outInfo.add(scrollPane2);
+		
 		
 		//tab.add(table1);
 	}
 	
 	public void Num(){
-		inNum = new JLabel("入库数量:");
 		Font font = new Font("宋体", Font.BOLD, 13);//BOLD PLAIN
+		
+		inNum = new JLabel("入库数量:");
 		inNum.setFont(font);
 		inNum.setBounds(panel.getWidth()*3/4,panel.getHeight()*81/100, panel.getWidth()*2/5, panel.getHeight()/15);
 		inNum.setVisible(true);
 		
-/*		outNum = new JLabel("出库数量");
+		outNum = new JLabel("出库数量");
 		outNum.setFont(font);
-		outNum.setBounds(panel.getWidth()*14/20,panel.getHeight()*49/60, panel.getWidth()*8/11, panel.getHeight()/15);
+		outNum.setBounds(panel.getWidth()*3/4,panel.getHeight()*81/100, panel.getWidth()*2/5, panel.getHeight()/15);
 		outNum.setVisible(true);
-	*/	
+	
 		allNum = new JLabel("存储数量合计:");
 		allNum.setFont(font);
 		allNum.setBounds(panel.getWidth()*71/100,panel.getHeight()*53/60, panel.getWidth()*2/5, panel.getHeight()/15);
@@ -355,22 +370,22 @@ public class ShowInventoryPanel {
 		inOfNum.setBounds(panel.getWidth()*34/40,panel.getHeight()*82/100, panel.getWidth()/18, panel.getHeight()/20);
 		inOfNum.setVisible(true);
 		
-	/*	outOfNum = new JTextField();
-		outOfNum.setBounds(panel.getWidth()*14/20,panel.getHeight()*49/60, panel.getWidth()*8/11, panel.getHeight()/15);
+		outOfNum = new JTextField();
+		outOfNum.setBounds(panel.getWidth()*34/40,panel.getHeight()*82/100, panel.getWidth()/18, panel.getHeight()/20);
 		outOfNum.setVisible(true);
-	*/
+	
 		all = new JTextField();
-		all.setBounds(panel.getWidth()*34/40,panel.getHeight()*89
-				/100, panel.getWidth()/18, panel.getHeight()/20);
+		all.setBounds(panel.getWidth()*34/40,panel.getHeight()*89/100, panel.getWidth()/18, panel.getHeight()/20);
 		all.setVisible(true);
 	    
 		
-		panel.add(inNum);
-		//panel.add(outNum);
+		inInfo.add(inNum);
+		outInfo.add(outNum);
+		inInfo.add(inOfNum);
+		outInfo.add(outOfNum);
 		panel.add(allNum);
-		panel.add(inOfNum);
-	//	panel.add(outOfNum);
 		panel.add(all);
+		
 	}
 
 	static public boolean isLeap(String a){
